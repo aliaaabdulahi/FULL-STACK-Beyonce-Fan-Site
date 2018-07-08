cd0d7292197b45540c6e31a19648fb028e9b24b7
Resilient Coders <resilient@Resilients-MacBook-Air-3.local>
2018-07-07 23:04:57 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://alia:alia11@ds227821.mlab.com:27821/savagelogin' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
